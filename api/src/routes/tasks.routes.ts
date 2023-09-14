36bb3603e5b5924c44494577c15832fc1d00bf88
Cachilox <marianopvp1@gmail.com>
2023-09-14 01:21:12 -0300

    feat(api/task schema): add validation schema for task creation

--- api/src/routes/tasks.routes.ts
+++ api/src/routes/tasks.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares";
 import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers";
+import { validateSchema } from "../middlewares";
+import { createTaskSchema } from "../schemas";
 
 const router = Router();
 
@@ -8,7 +10,7 @@ router.get("/tasks", authRequired, getTasks);
 
 router.get("/tasks/:id", getTask)
 
-router.post("/tasks", authRequired, createTask);
+router.post("/tasks", authRequired, validateSchema(createTaskSchema), createTask);
 
 router.put("/tasks/:id", authRequired, updateTask);
 
